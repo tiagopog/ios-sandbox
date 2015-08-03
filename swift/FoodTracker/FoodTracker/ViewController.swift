2f6b6cb3c733546deb6e898d2535a93267f6631d
Tiago Guedes <tiagopog@gmail.com>
2015-08-03 01:32:10 -0300

    Add new project FoodTracker #2

---
+++ swift/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Tiago Guedes on 8/2/15.
+//  Copyright © 2015 Tiago Guedes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
