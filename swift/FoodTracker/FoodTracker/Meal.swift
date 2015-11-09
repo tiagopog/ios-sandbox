6d53697597e5a103cf398fdf72fd36f51029ed66
Tiago Guedes <tiagopog@gmail.com>
2015-11-09 01:34:17 -0200

    FoodTracker - Add NSCoding protocol to Meal class

--- swift/FoodTracker/FoodTracker/Meal.swift
+++ swift/FoodTracker/FoodTracker/Meal.swift
@@ -9,12 +9,25 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
   // MARK: Properties
   
   var name: String
   var photo: UIImage?
   var rating: Int
+  
+  // MARK: Archiving paths
+  
+  static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+  static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+  
+  // MARK: Types
+  
+  struct PropertyKey {
+    static let nameKey = "name"
+    static let photoKey = "photo"
+    static let ratingKey = "rating"
+  }
   
   // MARK: Initialization
   
@@ -24,10 +37,38 @@ class Meal {
     self.photo = photo
     self.rating = rating
     
+    super.init()
+    
     // Initialization should fail if there is no name or if the rating is negative.
     if name.isEmpty || rating < 0 {
       return nil
     }
   }
+ 
+  // MARK: NSCoding
   
+  func encodeWithCoder(aCoder: NSCoder) {
+    aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+    aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+    aCoder.encodeObject(rating, forKey: PropertyKey.ratingKey)
+  }
+  
+  required convenience init?(coder aDecoder: NSCoder) {
+    /* The return value of decodeObjectForKey(_:) is AnyObject,
+       which is downcasted in the code above as a String */
+    let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+    
+    /* In this case, it downcastes using the optional type cast operator (as?),
+       because the photo property is an optional, so the value might be a UIImage,
+       or it might be nil */
+    let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+    
+    /* Because the return value of decodeIntegerForKey is Int, there’s no need to
+       downcast the decoded value. */
+    let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+    
+    // Must call designated initilizer.
+    self.init(name: name, photo: photo, rating: rating)
+    
+  }
 }
